444cc1425e6fec59b5d374ba0293ce22362f69ad
Long Liang <lngr23@gmail.com>
2020-04-17 13:18:21 -0700

    created actions and types for Queue.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,6 @@
 export { toggleModal, updateTeamSettings } from './appActions';
 export type { ModalAction, LoadingAction, TeamSettings } from './appActions';
 export { playersCountIncrement, playersCountDecrement, updatePlayersList } from './playersActions';
-export type {
-  CountIncrementAction,
-  CountDecrementAction,
-  updatePlayersListAction,
-  Player,
-  PlayersList
-} from './playersActions';
+export type { CountIncrementAction, CountDecrementAction, updatePlayersListAction, Player, PlayersList } from './playersActions';
+export {  updateQueueList, returnPlayerToLobby, queueCountDecrement, queueCountIncrement} from './queueActions';
+export type { QueueList, ReturnPlayerToLobby, QueueCountDecrement, QueueCountIncrement, MovePlayerToTeam } from './queueActions';
